d1d960d9583cd7e7be5094c5ee915317840d1ff6
davyson <davyssonlevi2.0@gmail.com>
2025-10-09 14:37:22 -0300

    resolvido2

--- 4-bimestre/ex01-loop-while/index.js
+++ 4-bimestre/ex01-loop-while/index.js
@@ -5,5 +5,5 @@ while(resultado < 10){
     console.log(resultado)
 }
 console.log(`a variavel "resultado" é igual a ${resultado}`)
-// === NÃO MODIFIQUE NADA ABAIXO ============
+// === NÃO MODIFIQUE NADA ABAIXO =============
 module.exports = resultado
