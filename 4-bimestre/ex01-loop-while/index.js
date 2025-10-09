377eb7c4faa392734ca49ba0850e76108db22a76
davyson <davyssonlevi2.0@gmail.com>
2025-10-09 14:36:23 -0300

    resolvido

--- 4-bimestre/ex01-loop-while/index.js
+++ 4-bimestre/ex01-loop-while/index.js
@@ -1,4 +1,9 @@
-// CRIE UMA LÓGICA AQUI
+let resultado = 0
 
+while(resultado < 10){
+    resultado++
+    console.log(resultado)
+}
+console.log(`a variavel "resultado" é igual a ${resultado}`)
 // === NÃO MODIFIQUE NADA ABAIXO ============
 module.exports = resultado
