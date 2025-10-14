47b665173a064e24600f284ca33f54b1e856be0e
davyson <davyssonlevi2.0@gmail.com>
2025-10-14 10:35:58 -0300

    resolvido

--- 4-bimestre/ex02-loop-do-while/index.js
+++ 4-bimestre/ex02-loop-do-while/index.js
@@ -1,4 +1,13 @@
 // CRIE UMA LÓGICA AQUI
+let contador = 5
+let resultado = 0
+
+do{
+    contador ++
+    resultado = contador
+    console.log(contador)
+}while(contador < 10)
+console.log(resultado)
 
 // === NÃO MODIFIQUE NADA ABAIXO ============
 module.exports = resultado
