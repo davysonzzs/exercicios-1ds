67dc5be21b91374038c425fc8789d01363ae1a80
davyson <davyssonlevi2.0@gmail.com>
2025-10-14 11:25:56 -0300

    resolvido

--- 4-bimestre/ex03-loop-for/index.js
+++ 4-bimestre/ex03-loop-for/index.js
@@ -1,4 +1,8 @@
 // CRIE UMA LÓGICA AQUI
-
+let resultado = 0
+for(let i = 1; resultado < 55; i++){
+    resultado = resultado + i
+}
+console.log(resultado)
 // === NÃO MODIFIQUE OU ADICIONE NADA ABAIXO ============
 module.exports = resultado
