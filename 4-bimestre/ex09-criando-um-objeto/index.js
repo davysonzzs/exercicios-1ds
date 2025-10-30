ad0d1c020d35457009fb1ba997f793f39a57b182
davyson <davylevi2.0@gmail.com>
2025-10-30 14:13:18 -0300

    exercios obj resolvidos

--- 4-bimestre/ex09-criando-um-objeto/index.js
+++ 4-bimestre/ex09-criando-um-objeto/index.js
@@ -1,4 +1,10 @@
 // CRIE UMA LÓGICA AQUI
-
+const produto = {
+    nome: "Teclado",
+    preco: 120,
+    estoque: 20,
+    categoria: "perifericos"
+}
+console.log(produto)
 // === NÃO MODIFIQUE OU ADICIONE NADA ABAIXO ============
 module.exports = { produto }
--- 4-bimestre/ex10-acessando-e-alterando-obj/index.js
+++ 4-bimestre/ex10-acessando-e-alterando-obj/index.js
@@ -1,4 +1,14 @@
 // CRIE UMA LÓGICA AQUI
+const carro = {
+    marca: "Toyota",
+    modelo: "corolla",
+    ano: 2020,
+    cor: "prata"
+}
 
+console.log(carro.modelo)
+carro.cor = "Preto"
+carro.preco = 120000
+console.log(carro)
 // === NÃO MODIFIQUE OU ADICIONE NADA ABAIXO ============
-module.exports = { produto }
+module.exports = { carro }
