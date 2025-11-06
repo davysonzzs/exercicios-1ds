016deb7e9632ed1ff16fe1b9a2edabb8787dd347
davyson <davylevi2.0@gmail.com>
2025-11-06 14:47:55 -0300

    resolvi

--- 4-bimestre/ex11-funcao-e-declarando-uma-funcao/index.js
+++ 4-bimestre/ex11-funcao-e-declarando-uma-funcao/index.js
@@ -1,4 +1,14 @@
 // CRIE UMA LÓGICA AQUI
+function boasVindas(){
+    let mensagem = "Bem-vindo(a) ao sistema!"
+    return mensagem
+}
+function soma(a, b){
+    return a + b
+}
 
+let msg = boasVindas()
+let cauculo = soma(5, 5)
+console.log(`${msg} \n ${cauculo}`)
 // === NÃO MODIFIQUE OU ADICIONE NADA ABAIXO ============
 module.exports = { boasVindas, soma }
